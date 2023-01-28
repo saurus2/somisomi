b2fad621bfa01bbeb51d4b30a1625d1a6d86806d
Jihyeok <bluesaurus2@gmail.com>
2023-01-28 15:50:42 -0800

    error

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
+import django_on_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -53,7 +55,8 @@ MIDDLEWARE = [
 ]
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
+django_on_heroku.settings(locals())
 
 ROOT_URLCONF = 'mysite.urls'
 
